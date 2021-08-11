5458bb1e9c0b2f0073e4011742911f94f9d7353e
kolchinvolodumur <KolchinVolo@gmail.com>
2021-08-12 01:50:36 +0300

    first commit

---
+++ src/app/index.ts
@@ -0,0 +1,3 @@
+export { default as renderCheckout } from './checkout/renderCheckout';
+export { default as renderOrderConfirmation } from './order/renderOrderConfirmation';
+export { initializeLanguageService } from './locale/getLanguageService';
